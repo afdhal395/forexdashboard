bc4fc47ae72a059a2acaae3b6fe98ca8435da57f
Afdhaluddin Afif Asha'ari <42729942+afdhal395@users.noreply.github.com>
2020-10-31 22:57:56 +0800

    consolidated table row into currency selection

--- index.php
+++ index.php
@@ -11,12 +11,45 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+
+    <?php
+        if ($_POST['forexpair']) {
+          $currencyselect = $_POST['forexpair'];
+        } else {
+          $currencyselect = "EURUSD";
+        }
+    ?>
+
     <div class="container">
+    <div>
+      <form name="currencypair" action="" method="post">
+        <div class="form-group-inline">
+          <label for="forexpair">Forex Pair</label>
+          <select class="form-control my-3" name="forexpair" id="forexpair" onchange="this.form.submit()">
+            <?php
+              $currencyList = array("EURUSD", "GBPUSD", "EURCHF", "EURGBP", "AUDUSD", "NZDUSD", "CHFJPY", "GBPJPY", "CADCHF", );
+              asort($currencyList);
+
+              foreach ($currencyList as $pair) {
+                if ($pair == $currencyselect) {
+                  echo "<option value='$pair' selected>$pair</option>";
+                } else {
+                  echo "<option value='$pair'>$pair</option>";
+                }
+              }
+            ?>
+            
+          </select>
+        </div>
+      </form>
+
+      
+    </div>
       <div class="row">
           <div class="col-md">
             <?php 
               $chart = new Chart;
-              $chart->symbol="EURUSD";
+              $chart->symbol="$currencyselect";
               $chart->interval="1h";
               $chart->showIntervalTabs="true";
               $chart->generate();
@@ -25,7 +58,7 @@
           <div class="col-md">
             <?php 
               $chart = new Chart;
-              $chart->symbol="EURUSD";
+              $chart->symbol="$currencyselect";
               $chart->interval="4h";
               $chart->generate();
             ?>
@@ -33,7 +66,7 @@
           <div class="col-md">
             <?php 
               $chart = new Chart;
-              $chart->symbol="EURUSD";
+              $chart->symbol="$currencyselect";
               $chart->interval="1D";
               $chart->generate();
             ?>
@@ -41,41 +74,7 @@
           <div class="col-md">
             <?php 
               $chart = new Chart;
-              $chart->symbol="EURUSD";
-              $chart->interval="1W";
-              $chart->generate();
-            ?>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md">
-          <?php 
-              $chart = new Chart;
-              $chart->symbol="GBPUSD";
-              $chart->interval="1h";
-              $chart->generate();
-            ?>
-          </div>
-          <div class="col-md">
-          <?php 
-              $chart = new Chart;
-              $chart->symbol="GBPUSD";
-              $chart->interval="4h";
-              $chart->generate();
-            ?>
-          </div>
-          <div class="col-md">
-          <?php 
-              $chart = new Chart;
-              $chart->symbol="GBPUSD";
-              $chart->interval="1D";
-              $chart->generate();
-            ?>
-          </div>
-          <div class="col-md">
-          <?php 
-              $chart = new Chart;
-              $chart->symbol="GBPUSD";
+              $chart->symbol="$currencyselect";
               $chart->interval="1W";
               $chart->generate();
             ?>
