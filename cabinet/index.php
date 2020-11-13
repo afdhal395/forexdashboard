f75d3595ba8c35113157a4f2ca0917794e46c27c
Afdhaluddin Afif Asha'ari <42729942+afdhal395@users.noreply.github.com>
2020-11-13 23:41:51 +0800

    added navbar

--- cabinet/index.php
+++ cabinet/index.php
@@ -25,6 +25,7 @@ function formatfloat($input, $precision){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+    <?php include('../includes/navbar.php') ?>
     <div class="mx-3 my-3"> <!-- padding horizontal and vertical rather than using container class -->
       <div class="alert alert-success" role="alert">
         <strong>InstaForex Account Status</strong>
